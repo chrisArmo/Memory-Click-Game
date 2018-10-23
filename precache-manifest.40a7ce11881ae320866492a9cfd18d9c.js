6e404d8b3bb1cef8db80de6e39162d46ff4beed9
Chris Armendarez <chris.michael.armo@gmail.com>
2018-10-23 15:41:01 -0700

    Updates

---
+++ precache-manifest.40a7ce11881ae320866492a9cfd18d9c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f101701d93bf858d900b",
+    "url": "/Memory-Click-Game/static/js/runtime~main.f101701d.js"
+  },
+  {
+    "revision": "45a84d8db6693ea4e0de",
+    "url": "/Memory-Click-Game/static/js/main.45a84d8d.chunk.js"
+  },
+  {
+    "revision": "d7a3d35d5aa43deaea55",
+    "url": "/Memory-Click-Game/static/js/1.d7a3d35d.chunk.js"
+  },
+  {
+    "revision": "45a84d8db6693ea4e0de",
+    "url": "/Memory-Click-Game/static/css/main.4e56a91a.chunk.css"
+  },
+  {
+    "revision": "b1baf43f2498256d4535f036a1949dfc",
+    "url": "/Memory-Click-Game/index.html"
+  }
+];
